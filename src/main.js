c09ecef5c54aed361b8be38230e06ca0d4135017
pxgamer <owzie123@gmail.com>
2018-05-08 14:38:03 +0100

    Update JS imports

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import bulma from 'bulma'
-import fontAwesome from '@fortawesome/fontawesome'
-import fontAwesomeFree from '@fortawesome/fontawesome-free-solid'
-
-// eslint-disable-next-line
-let forceIgnoreUnusedVariables = {bulma, fontAwesome, fontAwesomeFree}
+import 'bulma'
+import '@fortawesome/fontawesome'
+import '@fortawesome/fontawesome-free-solid'
 
 Vue.config.productionTip = false
 
