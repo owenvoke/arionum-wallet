0ff6fee8040074489d9ad192656e69d2ede1c0c4
pxgamer <owzie123@gmail.com>
2018-05-08 14:28:51 +0100

    Add imports for Bulma and FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import bulma from 'bulma'
+import fontAwesome from '@fortawesome/fontawesome'
+import fontAwesomeFree from '@fortawesome/fontawesome-free-solid'
+
+// eslint-disable-next-line
+let forceIgnoreUnusedVariables = {bulma, fontAwesome, fontAwesomeFree}
 
 Vue.config.productionTip = false
 
