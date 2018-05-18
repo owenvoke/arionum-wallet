610a18371d447ea26fc11151a84c2277cc4976b6
pxgamer <owzie123@gmail.com>
2018-05-18 17:08:12 +0100

    Add base58 API method

--- src/modules/Network.js
+++ src/modules/Network.js
@@ -1,6 +1,7 @@
 export default {
   endpoints: {
     address: '/api.php?q=getAddress',
+	base58: '/api.php?q=base58',
     balance: '/api.php?q=getPendingBalance'
   },
 
@@ -12,10 +13,13 @@ export default {
       .then(response => { return response.json() })
       .catch(error => console.error('Request failed', error))
   },
-  getBalance (account) {
-    return this.getRequest(this.endpoints.balance+`&account=${account}`)
-  },
   getAddress (publicKey) {
     return this.getRequest(this.endpoints.address+`&public_key=${publicKey}`)
+  },
+  getBase58 (data) {
+    return this.getRequest(this.endpoints.base58+`&data=${data}`)
+  },
+  getBalance (account) {
+    return this.getRequest(this.endpoints.balance+`&account=${account}`)
   }
 }
