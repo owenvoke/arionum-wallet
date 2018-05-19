d41b5472020c845f88c48af1892f2b55d567bc1d
pxgamer <owzie123@gmail.com>
2018-05-19 09:14:58 +0100

    Fix indentation in Network.js

--- src/modules/Network.js
+++ src/modules/Network.js
@@ -1,18 +1,18 @@
 export default {
   endpoints: {
     address: '/api.php?q=getAddress',
-	base58: '/api.php?q=base58',
+    base58: '/api.php?q=base58',
     balance: '/api.php?q=getBalance',
-	pendingBalance: '/api.php?q=getPendingBalance',
-	transactions: '/api.php?q=getTransactions',
-	transaction: '/api.php?q=getTransaction',
-	publicKey: '/api.php?q=getPublicKey',
-	generateAccount: '/api.php?q=generateAccount',
-	currentBlock: '/api.php?q=currentBlock',
-	block: '/api.php?q=getBlock',
-	blockTransactions: '/api.php?q=getBlockTransactions',
-	version: '/api.php?q=version',
-	mempoolSize: '/api.php?q=mempoolSize'
+    pendingBalance: '/api.php?q=getPendingBalance',
+    transactions: '/api.php?q=getTransactions',
+    transaction: '/api.php?q=getTransaction',
+    publicKey: '/api.php?q=getPublicKey',
+    generateAccount: '/api.php?q=generateAccount',
+    currentBlock: '/api.php?q=currentBlock',
+    block: '/api.php?q=getBlock',
+    blockTransactions: '/api.php?q=getBlockTransactions',
+    version: '/api.php?q=version',
+    mempoolSize: '/api.php?q=mempoolSize'
   },
 
   getPeer () {
@@ -24,25 +24,25 @@ export default {
       .catch(error => console.error('Request failed', error))
   },
   getAddress (publicKey) {
-    return this.getRequest(this.endpoints.address+`&public_key=${publicKey}`)
+    return this.getRequest(this.endpoints.address + `&public_key=${publicKey}`)
   },
   getBase58 (data) {
-    return this.getRequest(this.endpoints.base58+`&data=${data}`)
+    return this.getRequest(this.endpoints.base58 + `&data=${data}`)
   },
   getBalance (account) {
-    return this.getRequest(this.endpoints.balance+`&account=${account}`)
+    return this.getRequest(this.endpoints.balance + `&account=${account}`)
   },
   getPendingBalance (account) {
-    return this.getRequest(this.endpoints.pendingBalance+`&account=${account}`)
+    return this.getRequest(this.endpoints.pendingBalance + `&account=${account}`)
   },
   getTransactions (account) {
-    return this.getRequest(this.endpoints.transactions+`&account=${account}`)
+    return this.getRequest(this.endpoints.transactions + `&account=${account}`)
   },
   getTransaction (transaction) {
-    return this.getRequest(this.endpoints.transaction+`&transaction=${transaction}`)
+    return this.getRequest(this.endpoints.transaction + `&transaction=${transaction}`)
   },
   getPublicKey (account) {
-    return this.getRequest(this.endpoints.publicKey+`&account=${account}`)
+    return this.getRequest(this.endpoints.publicKey + `&account=${account}`)
   },
   generateAccount () {
     return this.getRequest(this.endpoints.generateAccount)
@@ -51,10 +51,10 @@ export default {
     return this.getRequest(this.endpoints.currentBlock)
   },
   getBlock (height) {
-    return this.getRequest(this.endpoints.block+`&height=${height}`)
+    return this.getRequest(this.endpoints.block + `&height=${height}`)
   },
   getBlockTransactions (height) {
-    return this.getRequest(this.endpoints.blockTransactions+`&height=${height}`)
+    return this.getRequest(this.endpoints.blockTransactions + `&height=${height}`)
   },
   version () {
     return this.getRequest(this.endpoints.version)
