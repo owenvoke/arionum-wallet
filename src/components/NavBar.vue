aa444682946bb01397c6c131258387974066662d
pxgamer <owzie123@gmail.com>
2018-05-19 11:59:30 +0100

    Remove unused hamburger menu for now

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,12 +5,6 @@
                 <img src="../assets/logo.svg" alt="" height="auto" width="28">
                 <div class="sub-text">Arionum Web Wallet</div>
             </a>
-
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
         </div>
 
         <div class="navbar-end">
