0f65f2f4de0eebf0e4280a3f563cabd0a0f9fc46
pxgamer <owzie123@gmail.com>
2018-05-30 11:44:13 +0100

    Add Block component

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Account from './components/Account'
+import Block from './components/Block'
 import Operations from './components/Operations'
 import Receive from './components/Receive'
 import Settings from './components/Settings'
 
 export default [
   {path: '/', component: Account},
+  {path: '/block/:blockHeight', component: Block, name: 'block'},
   {path: '/operations', component: Operations},
   {path: '/receive', component: Receive},
   {path: '/settings', component: Settings}
