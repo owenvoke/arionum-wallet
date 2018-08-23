6b35f8c82a87bc04401641013a80b52d6288443d
pxgamer <owzie123@gmail.com>
2018-08-23 13:27:30 +0100

    Add Bulma and Buefy

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import axios from 'axios'
+import Buefy from 'buefy'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'bulma/css/bulma.css'
+import 'buefy/lib/buefy.css'
 
 import App from './App'
 import router from './router'
@@ -9,6 +12,11 @@ import store from './store'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+// Vue plugins
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+})
 
 /* eslint-disable no-new */
 new Vue({
