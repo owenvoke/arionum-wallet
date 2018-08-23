2f6e63785d27bc6d0fa14653832f9b1547236afd
pxgamer <owzie123@gmail.com>
2018-08-23 13:27:52 +0100

    Update Vue templates to use Bulma

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.svg" alt="Arionum">
-    <main>
-      <div class="left-side">
-        <div>
-          <div class="title">Getting Started</div>
-          <br>
-          <button class="button">Select wallet file</button>
-          <button class="button">Enter keys manually</button>
-        </div>
+  <transition type="fade">
+    <section class="section has-text-centered">
+      <img class="logo" src="~@/assets/logo.svg" alt="Arionum">
+      <div class="wallet-section">
+        <h3 class="title is-3">Need a wallet?</h3>
+        <button class="button is-success">Create a new wallet</button>
       </div>
-    </main>
-  </div>
+      <div class="wallet-section">
+        <h3 class="title is-3">Already have a wallet?</h3>
+        <button class="button">Select wallet file</button>
+        <button class="button">Enter keys manually</button>
+      </div>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -25,57 +26,17 @@
   }
 </script>
 
-<style lang="scss">
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  body {
-    font-family: sans-serif;
-  }
-
-  #wrapper {
-    background: radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-    );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
-
-  #logo {
+<style lang="scss" scoped>
+  .logo {
     height: auto;
     margin-bottom: 20px;
     width: 420px;
   }
 
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #444;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #444;
+  .wallet-section {
+    margin-bottom: 1rem;
+    .button {
+      margin-bottom: 0.1rem;
+    }
   }
 </style>
