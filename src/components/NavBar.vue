511b64320865158185abce448e7b3fefe2abff43
pxgamer <owzie123@gmail.com>
2019-01-02 21:23:26 +0000

    Update the navbar formatting

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,16 +2,13 @@
   <b-navbar toggleable="md" type="dark" variant="dark">
 
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-    <b-navbar-brand to="/">Arionum Wallet</b-navbar-brand>
+    <b-navbar-brand to="/"><span class="text-info">Arionum</span> Wallet</b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
 
       <b-navbar-nav class="ml-auto">
         <b-nav-item right>
-          <b-button size="sm" variant="primary">Sign in</b-button>
-        </b-nav-item>
-        <b-nav-item right>
-          <b-button size="sm">Sign up</b-button>
+          <b-button size="sm" variant="secondary">Settings</b-button>
         </b-nav-item>
       </b-navbar-nav>
 
