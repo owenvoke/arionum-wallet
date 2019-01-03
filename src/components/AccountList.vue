e32c948000fe4ab2a9d7282e2a9b760dcdd40391
pxgamer <owzie123@gmail.com>
2019-01-03 12:27:31 +0000

    Update details section for account

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -2,11 +2,9 @@
   <div class="account-list">
     <h4 class="heading is-size-4 text-uppercase">Accounts</h4>
     <div class="border rounded p-3">
-      <div class="details">
-        <b-img rounded="circle" class="d-inline-block m-1" src="https://www.gravatar.com/avatar/369bfea86e43374e98de1b399994ec67?s=30&d=identicon"/>
-        <div class="d-inline align-middle">
-          <span class="text-monospace">51sJ4LbdKzhyGy4zJGqodNLse9n9JsVT2rdeH92w7cf3qQuSDJupvjbUT1UBr7r1SCUAXG97saxn7jt2edKb4v4J</span>
-        </div>
+      <div class="details d-block align-middle text-monospace text-truncate">
+        <b-img rounded="circle" class="d-inline-block m-1" src="https://www.gravatar.com/avatar/369bfea86e43374e98de1b399994ec67?s=30&d=identicon"></b-img>
+        <span>51sJ4LbdKzhyGy4zJGqodNLse9n9JsVT2rdeH92w7cf3qQuSDJupvjbUT1UBr7r1SCUAXG97saxn7jt2edKb4v4J</span>
       </div>
       <div class="mb-1">
         <span class="text-monospace balance">0.00044441 ARO</span>
