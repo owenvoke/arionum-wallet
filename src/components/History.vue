f8ed1ebccac8497094744801cf6b7f444be9fbb4
pxgamer <owzie123@gmail.com>
2019-01-03 14:47:17 +0000

    Move history to it's own component file

---
+++ src/components/History.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-4">
+    <h4 class="heading is-size-4 text-uppercase">History</h4>
+      <div class="border rounded">
+	    <b-input-group class="p-1">
+          <b-form-input placeholder="Search for amount or hash"></b-form-input>
+		  <b-button variant="outline-secondary" slot="append">
+            <i class="fas fa-search"></i>
+          </b-button>
+	    </b-input-group>
+      <b-table striped show-empty></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'history'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="home mt-4">
     <AccountList/>
-    <div class="mt-4">
-      <h4 class="heading is-size-4 text-uppercase">History</h4>
-      <div class="border rounded">
-	    <b-input-group class="p-1">
-          <b-form-input placeholder="Search for amount or hash"></b-form-input>
-		  <b-button variant="outline-secondary" slot="append">
-              <i class="fas fa-search"></i>
-          </b-button>
-	    </b-input-group>
-        <b-table striped show-empty></b-table>
-      </div>
-    </div>
+    <History/>
   </div>
 </template>
 
@@ -20,7 +9,8 @@
 export default {
   name: 'home',
   components: {
-    AccountList: require('@/components/AccountList.vue').default
+    AccountList: require('@/components/AccountList.vue').default,
+    History: require('@/components/History.vue').default
   }
 }
 </script>
