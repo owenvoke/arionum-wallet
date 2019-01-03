95f381a7f4223d7c8c68026271795a095de6fe67
pxgamer <owzie123@gmail.com>
2019-01-03 21:46:11 +0000

    Replace Vuex with local storage

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
-import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import '@fortawesome/fontawesome-free/js/all'
 import 'jdenticon/dist/jdenticon'
 import './registerServiceWorker'
+import './store'
 import './styles'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
