49bca62f07ed8437a1de270d0fc25ead1eb16b60
pxgamer <owzie123@gmail.com>
2019-01-03 15:14:29 +0000

    Fix typo in navbar id

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
     <b-navbar-toggle target="navCollapse"></b-navbar-toggle>
     <b-navbar-brand to="/"><span class="text-info">Arionum</span> Wallet</b-navbar-brand>
 
-    <b-collapse is-nav id="navCcollapse">
+    <b-collapse is-nav id="navCollapse">
 
       <b-navbar-nav class="ml-auto">
         <b-nav-item right>
