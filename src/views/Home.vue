a61e1a17c4771d0b613938203ca759bdeb561b4c
pxgamer <owzie123@gmail.com>
2019-01-03 14:44:38 +0000

    Remove transaction type filter for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,6 @@
       <h4 class="heading is-size-4 text-uppercase">History</h4>
       <div class="border rounded">
 	    <b-input-group class="p-1">
-          <b-dropdown text="All" variant="outline-secondary" slot="prepend">
-            <b-dropdown-item>All</b-dropdown-item>
-            <b-dropdown-item>In</b-dropdown-item>
-            <b-dropdown-item>Out</b-dropdown-item>
-          </b-dropdown>
           <b-form-input placeholder="Search for amount or hash"></b-form-input>
 		  <b-button variant="outline-secondary" slot="append">
               <i class="fas fa-search"></i>
