22aeda7dde80e0b0845d6ba913a039868950424c
pxgamer <owzie123@gmail.com>
2019-01-04 12:37:56 +0000

    Import and prototype the Aro config

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import './config/registerServiceWorker'
 import './config/store'
 import './config/styles'
 import App from './App.vue'
+import Aro from './config/app'
 
 Vue.config.productionTip = false
+Vue.prototype.$aro = Vue.$aro = Aro
 Vue.use(BootstrapVue)
 
 new Vue({
