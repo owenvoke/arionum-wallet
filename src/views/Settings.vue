dd8dfe19c4cb7029e8409bdf900779d1d617b677
pxgamer <owzie123@gmail.com>
2019-01-04 12:38:29 +0000

    Update Settings view to use the config

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,33 +2,16 @@
   <div class="settings mt-4">
     <h2>Settings</h2>
 
-  <b-form-group label="Node URI:">
-    <b-form-input v-model="settings.nodeUri"></b-form-input>
-  </b-form-group>
+    <b-form-group class="mt-3" label="Node URI:">
+      <b-form-input v-model="$aro.settings.node.uri" class="mb-2" placeholder="Enter node URI"></b-form-input>
+    </b-form-group>
 
-  <b-button @click="saveSettings" variant="primary">Save</b-button>
+    <b-button @click="$aro.saveSettings" variant="primary">Save</b-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'settings',
-  data () {
-    return {
-      settings: {
-        nodeUri: 'https://peer1.arionum.com'
-      }
-    }
-  },
-  mounted () {
-    if (this.$ls.get('settings')) {
-      this.settings = JSON.parse(this.$ls.get('settings'))
-    }
-  },
-  methods: {
-    saveSettings: function () {
-      this.$ls.set('settings', JSON.stringify(this.settings))
-    }
-  }
+  name: 'settings'
 }
 </script>
