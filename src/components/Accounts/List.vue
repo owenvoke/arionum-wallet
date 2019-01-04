1876d9cd001b042a0e992de50f2485a38505a0fa
pxgamer <owzie123@gmail.com>
2019-01-04 14:44:10 +0000

    Update accounts list to be dynamic

---
+++ src/components/Accounts/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="account-list">
+    <h4 class="heading is-size-4 text-uppercase">Accounts</h4>
+    <div class="border rounded p-3">
+      <div v-for="account in $aro.accounts" :key="account.address">
+        <Account :address="account.address"></Account>
+      </div>
+      <p v-if="!$aro.accounts || $aro.accounts.length === 0" class="text-center">There are no accounts to show</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Account from './Single'
+
+export default {
+  name: 'account-list',
+  components: { Account }
+}
+</script>
---
+++ src/components/Accounts/Single.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="account">
+    <div class="details d-block align-middle text-monospace text-truncate">
+      <svg class="d-inline-block m-1 rounded-circle" width="38" height="38"
+           :data-jdenticon-value="address"></svg>
+      <span v-text="address"></span>
+    </div>
+    <div class="mb-1">
+      <span class="text-monospace balance">0.00044441 ARO</span>
+    </div>
+    <div class="actions btn-group">
+      <b-dropdown size="sm" variant="outline-dark" no-caret>
+        <template slot="button-content">
+          <i class="fas fa-ellipsis-h fa-fw"></i>
+        </template>
+
+        <b-dropdown-item-button>
+          <i class="fas fa-save fa-fw mr-1"></i>
+          <span>Export</span>
+        </b-dropdown-item-button>
+        <b-dropdown-item-button>
+          <i class="fas fa-print fa-fw mr-1"></i>
+          <span>Print</span>
+        </b-dropdown-item-button>
+      </b-dropdown>
+      <b-button variant="secondary" size="sm">
+        <span>Send</span>
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'account',
+  props: {
+    address: String
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
